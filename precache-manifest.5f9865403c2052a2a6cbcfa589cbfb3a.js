7756119447c159e7fbe330b7dc3000ab927e1770
Alex Malichenko <malichenko-ov@ukrposhta.ua>
2020-08-05 12:29:29 +0300

    Updates

---
+++ precache-manifest.5f9865403c2052a2a6cbcfa589cbfb3a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b4016e8cfc22e3b54c3aef9ed9b05e",
+    "url": "/weather-widget/index.html"
+  },
+  {
+    "revision": "35adf6acb400810bc693",
+    "url": "/weather-widget/static/css/main.2fce6a8d.chunk.css"
+  },
+  {
+    "revision": "78943d89b53a6d37b7d1",
+    "url": "/weather-widget/static/js/2.33b9090e.chunk.js"
+  },
+  {
+    "revision": "ed5e70a04304973f823326e84a5026fa",
+    "url": "/weather-widget/static/js/2.33b9090e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35adf6acb400810bc693",
+    "url": "/weather-widget/static/js/main.56e6e2b2.chunk.js"
+  },
+  {
+    "revision": "1080c39aa6bd17d57543",
+    "url": "/weather-widget/static/js/runtime-main.85aef3ac.js"
+  },
+  {
+    "revision": "06825fa10944e31000ccb9183e6dad41",
+    "url": "/weather-widget/static/media/bg.06825fa1.png"
+  },
+  {
+    "revision": "eabf94951d3364dd79218e9c0fb84230",
+    "url": "/weather-widget/static/media/weather-icon-cloudy.eabf9495.png"
+  },
+  {
+    "revision": "608d6a01fe1677642d86e521a98cf3b4",
+    "url": "/weather-widget/static/media/weather-icon-rainy.608d6a01.png"
+  },
+  {
+    "revision": "5e112c6173192573527ab2ada4f6eed7",
+    "url": "/weather-widget/static/media/weather-icon-sunny.5e112c61.png"
+  }
+]);
